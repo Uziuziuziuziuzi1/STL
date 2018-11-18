68b20a2b1cdfedc2add6bea5ad43a29f96dc165b
Li Jiang <43282027+Uziuziuziuziuzi1@users.noreply.github.com>
2018-11-18 02:57:00 -0500

    Add files via upload

---
+++ templatelist.cpp
@@ -0,0 +1,19 @@
+#include "List.h"
+#include "Elephant.h"
+using namespace std;
+
+int main() {
+	List<int> a(10);
+	a.add(3);
+
+	cout << a << '\n';
+
+	List<float> b(20);
+	b.add(2.5f);
+
+	cout << b << '\n';
+
+	List<Elephant> c(5);
+	c.add(Elephant("James", 5));
+	cout << c;
+}
